be75ae4126f3e3d6c93704a27a5560b72e3b60ef
ahmed elamin <ahmedselamin@gmail.com>
2023-01-05 13:23:10 +0200

    added hash map, binary search tree

---
+++ hashMap.js
@@ -0,0 +1,58 @@
+//key value pair data structure
+//objects arre inherited off hash maps, except you can add your own key values in the object
+//application : constant time lookup and and insertion required eg database indexing
+
+class HashTable {
+  constructor(size) {
+    this.table = new Array(size);
+    this.size = size;
+  }
+
+  hash(key) {
+    let total = 0;
+    //traverse over key characters
+    for (let i = 0; i < key.length; i++) {
+      total += key.charCodeAt(i);
+    }
+    return total % this.size;
+  }
+
+  set(key, value) {
+    //obtaining the index where a value needs to be stored
+    const index = this.hash(key);
+    //store the value at index
+    this.table[index] = value;
+  }
+
+  get(key) {
+    //obtain index where the value of key is stored
+    const index = this.hash(key);
+    return this.table[index];
+  }
+
+  remove(key) {
+    //obtain index where value is at
+    const index = this.hash(key);
+    this.table[index] = undefined;
+  }
+
+  display() {
+    for (let i = 0; i < this.table.length; i++) {
+      if (this.table[i]) {
+        console.log(i, this.table[i]);
+      }
+    }
+  }
+}
+
+const table = new HashTable(50);
+
+table.set("name", "ahmed");
+table.set("age", 23);
+table.set("Job", "SWE");
+table.display();
+
+console.log(table.get("name"));
+
+table.remove("age");
+table.display();
